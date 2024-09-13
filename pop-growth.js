ab2b52bf481ff96fcaee4137a7f26ecf1fac960b
jvseds <jvaaltonen@gmail.com>
2024-09-13 17:53:52 +0300

    solved 3.1

--- pop-growth.js
+++ pop-growth.js
@@ -15,6 +15,16 @@ const initialPopulation = 1500;
 const growthRate = 5;
 const incomingMigrants = 100;
 const finalPopulation = 5000;
+
 let msg = `for initial pop = ${initialPopulation}, growth rate = ${growthRate}, migration = ${incomingMigrants}, it will take ${popInNYears(initialPopulation, growthRate, incomingMigrants, finalPopulation)} years to surpass population of ${finalPopulation}`;
+
+console.log(msg);
+
+const initialPopulation2 = 1500000;
+const growthRate2 = 2.5;
+const incomingMigrants2 = 10000;
+const finalPopulation2 = 2000000;
+
+msg = `for initial pop = ${initialPopulation2}, growth rate = ${growthRate2}, migration = ${incomingMigrants2}, it will take ${popInNYears(initialPopulation2, growthRate2, incomingMigrants2, finalPopulation2)} years to surpass population of ${finalPopulation2}`;
 
 console.log(msg);
