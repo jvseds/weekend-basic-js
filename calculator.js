8246a8580cee40e5835b07d0e15fed6df6392ad7
jvseds <jvaaltonen@gmail.com>
2024-09-13 16:39:04 +0300

    solved 2.7

--- calculator.js
+++ calculator.js
@@ -1,14 +1,24 @@
 // 2.7 basic calculator
-function basicOp (operation, val1, val2) {
+function basicOp (operator, val1, val2) {
     if (typeof val1 !== "number" || typeof val2 !== "number") {
         return "Error, at least one of the values is not a number";
     }
 
+    const operations = {
+        "+": (a, b) => a + b,
+        "-": (a, b) => a - b,
+        "*": (a, b) => a * b,
+        "/": (a, b) => {if (b !== 0) {return a / b;} else {return "can't divide by 0";}}
+    };
+
+    if (operations[operator]) {
+        return operations[operator](val1, val2);
+    } else {
+        return "Invalid operator!";
+    }
 }
 
-operations = {
-    "+": (a, b) => a + b,
-    "-": (a, b) => a - b,
-    "*": (a, b) => a * b,
-    "/": (a, b) => {if (b !== 0) return a / b}
-};
+const val1 = 5;
+const val2 = 2;
+const operator = "*";
+console.log(`${val1} ${operator} ${val2} = ${basicOp(operator, val1, val2)}`);
